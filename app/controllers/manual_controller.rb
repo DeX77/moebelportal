63c83d02d54de64c7e295390842074d579789648
Daniel Exner <dex@dragonslave.de>
2009-01-16 01:07:29 +0100

    Step Sortierfunktion

--- app/controllers/manual_controller.rb
+++ app/controllers/manual_controller.rb
@@ -13,11 +13,11 @@ class ManualController < ApplicationController
     #Aktuelles Manual
     @manual = @tm.topic_by_id(@id)
     #Alle Assoziationen
-    @all_assosciations = @manual.counterplayers()
+    @all_assosciations = @manual.counterplayers
     #Produkt zum Handbuch
     @product = @manual.counterplayers(:atype => @base_locator+"/association/manual_of")
     #Nötige Schritte
-    @set_of_steps = @manual.counterplayers(:atype => @base_locator+"/association/set_of_steps")
+    @set_of_steps = sort_by_order(@manual.counterplayers(:atype => @base_locator+"/association/set_of_steps"))
   end
   
   def create
@@ -40,4 +40,23 @@ class ManualController < ApplicationController
     @tm.destroy(@manual)
   end
   
+  def sort_by_order(set_of_steps)
+    return set_of_steps.sort() do |a,b|
+      if (a ==b)
+        0
+      else
+        aftera = a.counterplayers(:atype => @base_locator+"/association/sequence_of_steps", :otype => "do after")
+        aftera.each() do |newA| 
+          while (newA.counterplayers(:atype => @base_locator+"/association/sequence_of_steps", :otype => "do after"))
+            aftera +=newA.counterplayers(:atype => @base_locator+"/association/sequence_of_steps", :otype => "do after")
+            if(aftera.include?(b))
+              1
+            end
+          end
+        end        
+        -1        
+      end
+    end
+    
+  end
 end
