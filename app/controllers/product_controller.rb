f2e6667a31776a09fc8f444546927f1d45f1b302
Sven Krosse <svenkross@gmx.de>
2009-01-18 18:22:26 +0100

    see todo

--- app/controllers/product_controller.rb
+++ app/controllers/product_controller.rb
@@ -1,12 +1,20 @@
 class ProductController < ApplicationController
   
   def index
-    @products = @tm.get(@base_locator+"/types/product").instances
+    @type = @tm.get(@base_locator + "/types/product")
+    @products = @tm.get(@base_locator + "/types/product").instances
+
+    @manual_count = Hash.new
+    for product in @products
+      @manual_count[product.id] = product.counterplayers(:atype => @base_locator+"/association/manual_of").size
+    end
   end
   
   def show
     @id = params[:id].to_i
     @product = @tm.topic_by_id(@id)
+    @manuals = @product.counterplayers(:atype => @base_locator+"/association/manual_of")
+    @acc_manuals = @tm.get( @base_locator+"/association/manual_of")
   end
   def create
     
