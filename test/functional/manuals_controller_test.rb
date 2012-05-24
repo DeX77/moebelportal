bdb6b1f9cdbd54998d598ef906afa6b181c40f03
Daniel Exner <dex@dragonslave.de>
2012-05-24 23:20:25 +0200

    * also rename Testclass

--- test/functional/manuals_controller_test.rb
+++ test/functional/manuals_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class ManualControllerTest < ActionController::TestCase
+class ManualsControllerTest < ActionController::TestCase
   # Replace this with your real tests.
   test "the truth" do
     assert true
