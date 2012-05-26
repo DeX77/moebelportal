9086a453b056d36ecd234eb9dbef29dfa21fda39
Daniel Exner <dex@dragonslave.de>
2012-05-26 22:37:02 +0200

    * autogenerated path for features

--- features/support/paths.rb
+++ features/support/paths.rb
@@ -10,6 +10,30 @@ module NavigationHelpers
 
     when /the home\s?page/
       '/'
+    when /the new user page/
+      new_user_path
+
+    when /the new usergroup page/
+      new_usergroup_path
+
+    when /the new tool page/
+      new_tool_path
+
+    when /the new step page/
+      new_step_path
+
+    when /the new product page/
+      new_product_path
+
+    when /the new material page/
+      new_material_path
+
+    when /the new manual page/
+      new_manual_path
+
+    when /the new language page/
+      new_language_path
+
 
     # Add more mappings here.
     # Here is an example that pulls values out of the Regexp:
